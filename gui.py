53dcef4216fb7a413ebd845976e7aac9152be5cf
Khanh Nguyen <stevekhanh111098@gmail.com>
2023-12-04 13:10:45 -0600

    GUI design

--- gui.py
+++ gui.py
@@ -23,28 +23,30 @@ system_logo = ImageTk.PhotoImage(file="lms_logo.png")
 logo_widget = Label(root, image=system_logo, font=20, bg="#a8ceff")
 logo_widget.image = system_logo
 logo_widget.pack()
-logo_widget.grid(row=0, column=0, columnspan=2)
+logo_widget.grid(row=0, column=0, columnspan=2, pady=0)
+title = Label(root, text="Library Management System", font=10, bg="#a8ceff")
+title.grid(row=1, column=0, columnspan=2, pady=10, padx=100)
 
 
 #1  Check out new book loan
 check_out_btn = Button(root, text='Check Out', width=20)
-check_out_btn.grid(row=1, column=0, columnspan=2,pady=10, padx=100)
+check_out_btn.grid(row=2, column=0, columnspan=2,pady=10, padx=100)
 
 #2  Add new member into Borrower
 borrower_btn = Button(root, text='New Member', width=20)
-borrower_btn.grid(row=2, column=0, columnspan=2,pady=10, padx=100)
+borrower_btn.grid(row=3, column=0, columnspan=2,pady=10, padx=100)
 
 #3  Add new book into all 5 branches, 5 copies each branch
 new_book_btn = Button(root, text='New Book', width=20)
-new_book_btn.grid(row=3, column=0, columnspan=2,pady=10, padx=100)
+new_book_btn.grid(row=4, column=0, columnspan=2,pady=10, padx=100)
 
 #4  List the copies that are being loaned by the book title
 book_search_btn = Button(root, text='Search Book', width=20)
-book_search_btn.grid(row=4, column=0, columnspan=2,pady=10, padx=100)
+book_search_btn.grid(row=5, column=0, columnspan=2,pady=10, padx=100)
 
 #5  List late returned from book_loans by a given date
 late_list_btn = Button(root, text='Late List', width=20)
-late_list_btn.grid(row=5, column=0, columnspan=2,pady=10, padx=100)
+late_list_btn.grid(row=6, column=0, columnspan=2,pady=10, padx=100)
 
 
 
